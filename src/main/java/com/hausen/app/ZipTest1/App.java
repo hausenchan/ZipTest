649bec8dde8fe163552caf959c37d3b5f3f2ac6e
hausen <hausen.chan@gmail.com>
2013-08-25 16:36:05 +0800

    last commit

---
+++ src/main/java/com/hausen/app/ZipTest1/App.java
@@ -0,0 +1,13 @@
+package com.hausen.app.ZipTest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
